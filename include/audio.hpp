830314b73a2fcb2375a22e62d25077a1317f7a18
a22Dv <a22Dv.gl@gmail.com>
2026-02-01 21:25:14 +0800

    Implemented AudioDevice.

--- include/audio.hpp
+++ include/audio.hpp
@@ -4,25 +4,37 @@ extern "C" {
 #include <miniaudio.h>
 }
 
+#include <atomic>
 #include <new>
 #include <span>
 #include <vector>
-#include <atomic>
 
 #include "types.hpp"
 
 #define MDM_ALIGNAS_HDI alignas(std::hardware_destructive_interference_size)
 
+#pragma warning(push)
+#pragma warning(disable : 4324)
+
 namespace mdm {
 
 using f32 = float;
 
-constexpr i32 sample_rate = 48000;
-constexpr i32 channels = 2;
+constexpr i32 sample_rate = 44100;
+constexpr i32 channels = 1;
 
 /// @brief Audio device that wraps miniaudio internals. Supports in/out.
+/// @note Non-copyable, non-movable.
 class AudioDevice {
    public:
+    AudioDevice();
+    ~AudioDevice();
+
+    AudioDevice(const AudioDevice &) = delete;
+    AudioDevice operator=(const AudioDevice &) = delete;
+    AudioDevice(AudioDevice &&) = delete;
+    AudioDevice operator=(AudioDevice &&) = delete;
+
     /// @brief Read a given number of samples from the microphone for the full length of span.
     /// @note This is a blocking operation and will fill the given span.
     void read(std::span<f32> samples);
@@ -33,16 +45,17 @@ class AudioDevice {
 
    private:
     /// @brief Internal miniaudio callback.
-    static void _callback(ma_device* device, void* out, void* in, ma_uint32 sample_count);
+    static void _callback(ma_device *device, void *out, const void *in, ma_uint32 sample_count);
 
     ma_device _device{};
-    MDM_ALIGNAS_HDI std::atomic<usize> _in_head = 0; // Writer.
-    MDM_ALIGNAS_HDI std::atomic<usize> _out_head = 0; // Writer.
-    MDM_ALIGNAS_HDI std::atomic<usize> _in_tail = 0; // Reader.
-    MDM_ALIGNAS_HDI std::atomic<usize> _out_tail = 0; // Reader.
-    MDM_ALIGNAS_HDI std::atomic<bool> _terminate = false;
-    std::vector<f32> _out_samples{usize(sample_rate / 2), 0.0f};
-    std::vector<f32> _in_samples{usize(sample_rate / 2), 0.0f};
+    MDM_ALIGNAS_HDI std::atomic<usize> _in_head = 0;   
+    MDM_ALIGNAS_HDI std::atomic<usize> _out_head = 0;  
+    MDM_ALIGNAS_HDI std::atomic<usize> _in_tail = 0;   
+    MDM_ALIGNAS_HDI std::atomic<usize> _out_tail = 0; 
+    std::vector<f32> _out_samples = std::vector<f32>(usize(sample_rate / 4), 0.0f);
+    std::vector<f32> _in_samples = std::vector<f32>(usize(sample_rate / 4), 0.0f);
 };
 
 }  // namespace mdm
+
+#pragma warning(pop)
--- src/audio.cpp
+++ src/audio.cpp
@@ -9,12 +9,37 @@ extern "C" {
 
 #include <algorithm>
 #include <span>
+#include <stdexcept>
 #include <vector>
 
 #include "audio.hpp"
 #include "types.hpp"
 
 namespace mdm {
+
+AudioDevice::AudioDevice() {
+    ma_device_config config = ma_device_config_init(ma_device_type_duplex);
+    config.capture.channels = channels;
+    config.playback.channels = channels;
+    config.capture.format = ma_format_f32;
+    config.playback.format = ma_format_f32;
+    config.sampleRate = sample_rate;
+    config.pUserData = this;
+    config.dataCallback = _callback;
+    ma_result initr = ma_device_init(NULL, &config, &_device);
+    if (initr != MA_SUCCESS) {
+        throw std::runtime_error("[ERROR] Device initialization failure.");
+    }
+    ma_result strtr = ma_device_start(&_device);
+    if (strtr != MA_SUCCESS) {
+        throw std::runtime_error("[ERROR] Device startup failure.");
+    }
+}
+
+AudioDevice::~AudioDevice() {
+    ma_device_stop(&_device);
+    ma_device_uninit(&_device);
+}
 
 void AudioDevice::read(std::span<f32> samples) {
     using mem = std::memory_order;
@@ -26,8 +51,8 @@ void AudioDevice::read(std::span<f32> samples) {
     while (samples_read < sample_count) {
         const usize w_idx = _in_head.load(mem::acquire);
         const usize r_idx = _in_tail.load(mem::relaxed);
-        const usize dist = (buffer_size * (w_idx < r_idx)) - r_idx + w_idx;
-        if (!dist) {
+        const usize dist = (buffer_size * (w_idx < r_idx)) + w_idx - r_idx;
+        if (w_idx == r_idx) {
             _in_head.wait(w_idx, mem::acquire);
             continue;
         }
@@ -38,8 +63,78 @@ void AudioDevice::read(std::span<f32> samples) {
     }
 }
 
-/// TODO: Implementation.
 void AudioDevice::write(std::span<const f32> samples) {
+    using mem = std::memory_order;
+
+    const usize buffer_size = _out_samples.size();
+    const usize samples_count = samples.size();
+
+    usize samples_written = 0;
+    while (samples_written < samples_count) {
+        const usize w_idx = _out_head.load(mem::relaxed);
+        const usize r_idx = _out_tail.load(mem::acquire);
+        if ((w_idx + 1) % buffer_size == r_idx) {
+            _out_tail.wait(r_idx, mem::acquire);
+            continue;
+        }
+        const usize available = buffer_size * (r_idx <= w_idx) + r_idx - w_idx;
+        const usize write_count =
+            std::min({available - 1, buffer_size - w_idx, samples_count - samples_written});
+        std::copy_n(samples.begin() + samples_written, write_count, _out_samples.begin() + w_idx);
+        _out_head.store((w_idx + write_count) % buffer_size, mem::release);
+        samples_written += write_count;
+    }
+}
+
+void AudioDevice::_callback(ma_device *device, void *out, const void *in, ma_uint32 sample_count) {
+    using mem = std::memory_order;
+    AudioDevice &adev = *static_cast<AudioDevice *>(device->pUserData);
+
+    const usize out_buffer_size = adev._out_samples.size();
+    const usize in_buffer_size = adev._in_samples.size();
+    const f32 *inf = static_cast<const f32 *>(in);
+    f32 *outf = static_cast<f32 *>(out);
+
+    bool processed = false;
+    usize samples_processed = 0;
+    while (samples_processed < sample_count) {
+        const usize out_w_idx = adev._out_head.load(mem::acquire);
+        const usize out_r_idx = adev._out_tail.load(mem::relaxed);
+        const usize available = out_buffer_size * (out_w_idx < out_r_idx) + out_w_idx - out_r_idx;
+        if (!available) {
+            std::fill(outf + samples_processed, outf + sample_count, 0.0f);
+            break;
+        }
+        const usize write_count =
+            std::min({available, out_buffer_size - out_r_idx, sample_count - samples_processed});
+        std::copy_n(adev._out_samples.begin() + out_r_idx, write_count, outf + samples_processed);
+        adev._out_tail.store((out_r_idx + write_count) % out_buffer_size, mem::release);
+        samples_processed += write_count;
+        processed = true;
+    }
+    if (processed) {
+        adev._out_tail.notify_one();
+    }
+
+    processed = false;
+    samples_processed = 0;
+    while (samples_processed < sample_count) {
+        const usize in_w_idx = adev._in_head.load(mem::relaxed);
+        const usize in_r_idx = adev._in_tail.load(mem::acquire);
+        const usize available = in_buffer_size * (in_r_idx <= in_w_idx) + in_r_idx - in_w_idx;
+        if (!available) {
+            break;
+        }
+        const usize read_count =
+            std::min({available, in_buffer_size - in_w_idx, sample_count - samples_processed});
+        std::copy_n(inf + samples_processed, read_count, adev._in_samples.begin() + in_w_idx);
+        adev._in_head.store((in_w_idx + read_count) % in_buffer_size, mem::release);
+        samples_processed += read_count;
+        processed = true;
+    }
+    if (processed) {
+        adev._in_head.notify_one();
+    }
 }
 
 }  // namespace mdm
